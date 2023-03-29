b3bf92e427e57691bf3e88cf32b86e38663c53ba
KellsCodes <nworjiifeanyi@gmail.com>
2023-03-29 08:08:37 +0000

    fixed bug on file creation with puppet

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 # creates a file in /tmp
 
 file { '/tmp/school':
-content => 'I love puppet',
+ensure  => 'file',
+content => 'I love Puppet',
 mode    => '0744',
 owner   => 'www-data',
 group   => 'www-data',
