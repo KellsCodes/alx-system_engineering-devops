87307f2f40b4d5284d20156fd59da75e00136d88
KellsCodes <nworjiifeanyi@gmail.com>
2023-03-30 04:48:09 +0000

    fixed bug on puppet ssh configuration file

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -11,6 +11,6 @@ file_line { 'Turn off passwd auth':
 file_line { 'Declare identity file':
 	ensure  => present,
 	path    => '/etc/ssh/ssh_config',
-	line    => '    IdentityFile ~/.sh/school',
+	line    => '    IdentityFile ~/.ssh/school',
 	replace => true,
 }
